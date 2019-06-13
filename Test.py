f06455e51b080b9dce95c46e98c08588a4a4924c
406096917@qq.com <406096917@qq.com>
2019-06-13 19:15:32 +0800

    2019年6月13日19:08:00

--- Test.py
+++ Test.py
@@ -1,6 +1,13 @@
 #coding = utf-8
 #防止中文显示乱码
 #coding = gb18030
+from git import Repo
+
+repo = Repo.init('E:\Peiyinxiu_appium\Peiyinxiu_Client')
+repo.index.add(['Test.py'])
+repo.index.commit('2019年6月13日19:08:00')
+remote = repo.remote()
+remote.push('master')
 
 
 
