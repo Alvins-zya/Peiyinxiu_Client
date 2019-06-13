c89e31a3921dd8a055f4d50aadaa6d126059d60a
406096917@qq.com <406096917@qq.com>
2019-06-13 18:54:32 +0800

    2019年6月13日18:54:28

--- Test.py
+++ Test.py
@@ -2,19 +2,5 @@
 #防止中文显示乱码
 #coding = gb18030
 
-import os
-import time
-os.system('chcp 65001')
-time.sleep(1)
-os.system('e:')
-time.sleep(1)
-os.system('cd Peiyinxiu_appium\Peiyinxiu_Client ')
-time.sleep(1)
-dir = os.system('dir')
-print(dir)
-os.system('git add .')
-time.sleep(5)
-os.system('git commit -m "%s"'%(time.time()))
-time.sleep(3)
-os.system('git push -u origin master ')
+
 
