bbfca923c658ad0e594263268d89744d6e674d18
Alvins-zya <48586279+Alvins-zya@users.noreply.github.com>
2020-05-02 12:26:00 +0800

    2020年5月2日12:25:53

--- Dubbing9_11/test_Source_Library.py
+++ Dubbing9_11/test_Source_Library.py
@@ -528,8 +528,146 @@ class Test_c_Voicetest(Dubbing):
             time.sleep(2)
         self.driver.find_id(soucred_id + 'reIndetify').click()
         time.sleep(2)
+        #返回素材库
         self.driver.find_id(soucred_id + 'back').click()
         time.sleep(2)
+
+class Test_d_source_list(Dubbing):
+    #素材列表
+    def test_a_list(self):
+        self.driver.find_id(soucred_id + 'unlimited').click()
+        time.sleep(2)
+        self.driver.find_xpath('热门').click()
+        time.sleep(2)
+        for i in range(15):
+            self.driver.swip_up()
+            time.sleep(2)
+        time.sleep(2)
+        try:
+            self.driver.find_id(soucred_id + 'top_img')
+            self.driver.find_id(soucred_id + 'top_img').click()
+        except:
+            pass
+        time.sleep(4)
+
+    #切换角色筛选项
+    def test_b_select_role(self):
+        self.driver.find_id(soucred_id + 'boy').click()
+        time.sleep(2)
+        try:
+            self.driver.find_id(soucred_id + 'tv_girl')
+        except:
+            pass
+        time.sleep(2)
+        self.driver.find_id(soucred_id + 'coor').click()
+        time.sleep(2)
+        num = self.driver.find_ids(soucred_id + 'tv_source_from')
+        for i in range(len(num)):
+            try:
+                self.driver.find_ids(soucred_id + 'tv_boy')[i]
+            except:
+                name = self.driver.find_ids(soucred_id + 'tv_source_title')[i].text
+                raise ("合作素材列表中未显示双人角色:%s"%name)
+            time.sleep(2)
+        time.sleep(2)
+        self.driver.find_id(soucred_id + 'unlimited').click()
+        time.sleep(4)
+
+
+class Test_e_Preview(Dubbing):
+    #点击进入素材预览界面
+    def test_a_touch_source(self):
+        source_num = self.driver.find_ids(soucred_id + 'iv_source')
+        for i in range(len(source_num)):
+            self.driver.find_ids(soucred_id + 'iv_source')[i].click()
+            try:
+                self.driver.wait_id(soucred_id + 'userhead')
+                self.driver.Background()
+                time.sleep(2)
+                self.driver.find_id(soucred_id + 'btnBack').click()
+                time.sleep(2)
+            except:
+                self.driver.find_id(soucred_id + 'btnReload')
+                self.driver.find_id(soucred_id +'btnReload').click()
+                time.sleep(2)
+                self.driver.find_id(soucred_id + 'btnBack').click()
+                time.sleep(2)
+            time.sleep(2)
+        time.sleep(2)
+    #素材预览详情界面查看视频是否循环播放
+    def test_b_play(self):
+        self.driver.find_id(soucred_id + 'iv_source').click()
+        self.driver.wait_id(soucred_id + 'userhead')
+        time.sleep(50)
+        try:
+            self.driver.find_id(soucred_id + 'play')
+            raise ('素材视频未循环播放')
+        except:
+            self.driver.Background()
+            time.sleep(2)
+            self.driver.wait_id(soucred_id + 'play')
+            time.sleep(2)
+
+    #视频暂停情况下拖动进度条
+    def test_e(self):
+        if self.y == 1920:
+            self.driver.swip_move(self.x * 0.202,self.y * 0.471,self.x * 0.566,self.y * 0.471)
+        elif self.y > 2250:
+            pass
+        else:
+            pass
+        time.sleep(2)
+        self.driver.find_id(soucred_id + 'play').click()
+        time.sleep(20)
+        self.driver.Background()
+        time.sleep(2)
+
+    #进入素材作者个人空间
+    def test_f(self):
+        name1 = self.driver.find_id(soucred_id + 'user_name').text
+        self.driver.find_id(soucred_id + 'userhead').click()
+        self.driver.wait_id(soucred_id + 'fanscount')
+        name2 = self.driver.find_id(soucred_id + 'username').text
+        self.assertEqual(name1,name2,msg='素材视频详情界面的用户名称与空间中的用户名称校验不一致')
+        time.sleep(2)
+        self.driver.find_id(soucred_id + 'btnBack').click()
+        time.sleep(2)
+
+    #关注按钮
+    def test_g(self):
+        try:
+            self.driver.find_id(soucred_id + 'userhead')
+            self.driver.find_id(soucred_id + 'btnBack').click()
+        except:
+            pass
+        time.sleep(2)
+        self.driver.find_id(soucred_id + 'btn_video_detail_follow').click()
+        try:
+            follow_toast = self.driver.wait_toast('//android.widget.Toast')
+            check = '关注成功'
+            self.assertEqual(follow_toast,check,msg='关注toast内容校验不一致')
+        except:
+            try:
+                self.driver.find_id(soucred_id + 'editContent')
+                time.sleep(2)
+                self.driver.find_id(soucred_id + 'btnBack').click()
+            except:
+                pass
+        time.sleep(2)
+        self.driver.find_id(soucred_id + 'userhead').click()
+        self.driver.wait_id(soucred_id + 'fanscount')
+        self.driver.find_id(soucred_id + 'follow_status').click()
+        time.sleep(4)
+        self.driver.find_id(soucred_id + 'btnBack').click()
+        time.sleep(2)
+
+    #点击素材标签
+    def test_h(self):
+
+
+
+
+
 
 
 
