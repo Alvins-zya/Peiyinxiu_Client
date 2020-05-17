adbf6293a0d04e08b879afdf8cc4c984bf0e8e18
Alvins-zya <48586279+Alvins-zya@users.noreply.github.com>
2020-05-17 16:15:31 +0800

    2020年5月2日10:46:04

--- TEST/Test4.py
+++ TEST/Test4.py
@@ -1,14 +1,14 @@
 import unittest
-import re
-import random
-from selenium import webdriver
+# import re
+# import random
+# from selenium import webdriver
 
 class Test(unittest.TestCase):
 
     def test(self):
-        list = ['念白/旁白', '情感语录', '抖音', 'Pia戏', '影视混剪', '古风/仙侠', '萌新', '情感网文', '古风', '萌萌哒', '朗诵', '动漫', '王者荣耀', '桀儿自制', '兔子温柔馆', '朗读', '优酱自制', '声优', '星子自制']
-        list.remove('Pia戏')
-        print(list)
+        list = '34'
+        list1 = '35.0'
+        assert list in list1
 
 
 if __name__ == '__main__':
