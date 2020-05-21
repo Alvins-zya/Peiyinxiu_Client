96840262e26bcbb3c6909c63db292c77cbdb7c79
Alvins-zya <48586279+Alvins-zya@users.noreply.github.com>
2020-05-21 20:52:03 +0800

    2020年4月17日18:32:02

--- Dubbing9_11/Front.py
+++ Dubbing9_11/Front.py
@@ -2,7 +2,9 @@
 import unittest
 import warnings
 
-
+curPath = os.path.abspath(os.path.dirname(__file__))
+rootPath = os.path.split(curPath)[0]
+sys.path.append(rootPath)
 from operate.Driver_Operate import BaseOperate
 
 
