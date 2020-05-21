3b406b3bba121a53963eac97acd8cb60dc3e76ef
Alvins-zya <48586279+Alvins-zya@users.noreply.github.com>
2020-05-21 22:49:14 +0800

    2020年4月17日18:32:02

--- Dubbing9_11/test_Home_follow.py
+++ Dubbing9_11/test_Home_follow.py
@@ -4,6 +4,11 @@ import random
 import pytest
 import time
 import re
+import sys
+import os
+curPath = os.path.abspath(os.path.dirname(__file__))
+rootPath = os.path.split(curPath)[0]
+sys.path.append(rootPath)
 from collections import Counter
 from Dubbing9_11.Front import Dubbing
 sourced_id = 'com.happyteam.dubbingshow:id/'
