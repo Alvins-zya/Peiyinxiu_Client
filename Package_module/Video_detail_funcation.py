b5e797e7b21e3ab3a94a0df1ecabf521c79dd8bc
Alvins-zya <48586279+Alvins-zya@users.noreply.github.com>
2020-08-06 21:41:58 +0800

    2020年8月6日21:41:53

--- Package_module/Video_detail_funcation.py
+++ Package_module/Video_detail_funcation.py
@@ -21,11 +21,13 @@ class Video_detail_function():
 
     # 点击用户头像进入个人空间
     def Head_into_zoom_back(self):
-        self.driver.find_id(self.id + 'userhead').click()
-        self.driver.wait_id(self.id + 'fanscount')
-        time.sleep(2)
-        self.driver.find_id(self.id + 'btnBack').click()
-        time.sleep(4)
+        Heads = self.driver.find_ids(self.id + 'userhead')
+        for i in range(len(Heads)):
+            self.driver.find_ids(self.id + 'userhead')[i].click()
+            self.driver.wait_id(self.id + 'fanscount')
+            time.sleep(2)
+            self.driver.find_id(self.id + 'btnBack').click()
+            time.sleep(3)
 
     # 关注
     def Video_follow(self):
@@ -75,7 +77,7 @@ class Video_detail_function():
     def Video_good(self):
         num = self.driver.find_id(self.id + 'tv_good').text
         try:
-            self.driver.find_id(self.id + + 'good_svga').click()
+            self.driver.find_id(self.id + 'good_svga').click()
             tip = self.driver.wait_toast('//android.widget.Toast')
             check = '恭喜你获得300金币'
             assert tip == check
@@ -178,7 +180,7 @@ class Video_detail_function():
             self.driver.find_id(self.id + 'free_count').click()
             exp_toast = self.driver.wait_toast('//android.widget.Toast')
             check = '曝光机会用完'
-            self.assertIn(check, exp_toast)
+            assert check == exp_toast
             return None
         self.driver.find_id(self.id + 'free_count').click()
         time.sleep(2)
@@ -296,152 +298,53 @@ class Video_detail_function():
             self.driver.find_id(self.id + 'coor').click()
             while True:
                 try:
-                    self.driver.find_id(self.id + 'action')
+                    #素材下载失败/素材已被删除
+                    self.driver.wait_toast('//android.widget.Toast')
                     break
                 except:
                     try:
-                        self.driver.find_id(self.id + 'btnSubmit')
-                        self.driver.find_id(self.id + 'btnSubmit').click()
+                        self.driver.find_id(self.id + 'action')
                         break
                     except:
-                        pass
+                        #限制素材进入配音界面显示限制弹窗
+                        try:
+                            self.driver.find_id(self.id + 'btnSubmit')
+                            self.driver.find_id(self.id + 'btnSubmit').click()
+                            break
+                        except:
+                            pass
             time.sleep(2)
-            self.driver.find_id(self.id + 'action').click()
-            self.driver.find_id(self.id + 'close').click()
+            self.driver.find_id(self.id + 'back').click()
             time.sleep(2)
+            self.driver.find_id(self.id + 'btnSubmit').click()
+            time.sleep(3)
         except Exception as e:
             raise e
         time.sleep(2)
-        try:
-            self.driver.find_id(self.id + 'close')
-            self.driver.find_id(self.id + 'close').click()
-        except:
-            pass
+        self.driver.find_id(self.id + 'close').click()
         time.sleep(2)
 
-    # 点击原声素材配音完成后保存草稿箱
-    def test_i_a(self):
+    # 视频详情界面进入素材预览界面
+    def Video_source(self):
         self.driver.find_id(self.id + 'tvSource').click()
         self.driver.wait_id(self.id + 'source_title')
         self.driver.Background()
         time.sleep(2)
-        self.driver.find_id(self.id + 'dubbing_fake').click()
-        while True:
-            try:
-                self.driver.find_id(self.id + 'roleall')
-                break
-            except:
-                try:
-                    self.driver.find_id(self.id + 'btnSubmit')
-                    tip = self.driver.find_id(self.id + 'txtTitle').text
-                    print(tip)
-                    self.driver.find_id(self.id + 'btnSubmit').click()
-                    break
-                except:
-                    pass
-        time.sleep(2)
-        self.driver.find_id(self.id + 'roleall').click()
-        time.sleep(2)
-        self.driver.find_id(self.id + 'action').click()
-        self.driver.wait_download(self.id + 'title')
-        self.driver.Background()
-        time.sleep(2)
-        self.driver.find_id(self.id + 'complete').click()
-        self.driver.wait_id(self.id + 'txtTitle')
-        time.sleep(2)
-        self.driver.find_id(self.id + 'pri_switch_tv').click()
-        time.sleep(2)
-        self.driver.find_id(self.id + 'saveToDraft').click()
-        time.sleep(2)
-        self.driver.find_id(self.id + 'btnSubmit').click()
-        self.driver.wait_xpath('退出配音')
-        self.driver.find_xpath('退出配音').click()
-        time.sleep(2)
-        self.driver.wait_id(self.id + 'userhead')
-        time.sleep(2)
         self.driver.find_id(self.id + 'btnBack').click()
         time.sleep(4)
-
-    # 配音完成后保存草稿箱
-    def test_i_b(self):
-        while True:
-            try:
-                self.driver.find_id(self.id + 'tvSource')
-                break
-            except:
-                self.driver.find_id(self.id + 'btnBack').click()
-                time.sleep(2)
-                self.driver.swip_up()
-                time.sleep(2)
-                self.driver.find_id(self.id + 'film_img2').click()
-                self.driver.wait_id(self.id + 'follow_ta')
-                self.driver.Background()
-                time.sleep(2)
-        time.sleep(2)
-        self.driver.find_id(self.id + 'tvSource').click()
-        self.driver.wait_id(self.id + 'types_name')
-        self.driver.Background()
-        time.sleep(2)
-
-    # 素材预览界面配音
-    def Video_source_dub(self):
-        self.driver.find_id(self.id + 'dubbing_fake').click()
-        while True:
-            try:
-                self.driver.find_id(self.id + 'action')
-                break
-            except:
-                try:
-                    self.driver.find_id(self.id + 'btnSubmit')
-                    self.driver.find_id(self.id + 'btnSubmit').click()
-                    try:
-                        self.driver.find_id(self.id + 'roleall')
-                        self.driver.find_id(self.id + 'roleall').click()
-                    except:
-                        pass
-                    break
-                except:
-                    try:
-                        self.driver.find_id(self.id + 'roleall')
-                        self.driver.find_id(self.id + 'roleall').click()
-                        break
-                    except:
-                        pass
-        time.sleep(2)
-        self.driver.find_id(self.id + 'action').click()
-        self.driver.wait_download(self.id + 'title')
-        self.driver.Background()
-        time.sleep(2)
-        self.driver.find_id(self.id + 'complete').click()
-        self.driver.wait_id(self.id + 'txtTitle')
-        time.sleep(2)
-        self.driver.find_id(self.id + 'pri_switch_tv').click()
-        time.sleep(2)
-        self.driver.find_id(self.id + 'saveToDraft').click()
-        time.sleep(4)
-        self.driver.find_id(self.id + 'btnSubmit').click()
-        time.sleep(2)
-        self.driver.wait_id(self.id + 'btnCancel')
-        try:
-            self.driver.find_xpath('退出配音')
-            self.driver.find_xpath('退出配音').click()
-        except:
-            pass
-        self.driver.wait_id(self.id + 'userhead')
-        time.sleep(2)
-        self.driver.find_id(self.id + 'btnBack').click()
-        time.sleep(2)
 
     # 作品分享
     def Video_share(self):
         self.driver.find_id(self.id + 'tv_share').click()
         time.sleep(2)
+
         # 朋友圈
         if self.y == 1920:
             self.driver.tap(self.x * 0.12, self.y * 0.68)
         self.driver.wait_xpath('发表')
         self.driver.find_id('com.tencent.mm:id/dn').click()
         time.sleep(3)
+
         # QQ空间
         self.driver.find_id(self.id + 'tv_share').click()
         time.sleep(2)
